9b1680097fb414a21b176028ddb017dc289ba378
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-14 22:23:30 +0900

    ファイルを読み込んで描画するComponent

---
+++ frontend/src/components/molecules/ImageView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="image-view">
+        <FileInput  @on-load="onReadFile" />
+        <ImageArea :image="image" />
+    </div>
+</template>
+
+<script>
+import FileInput from '@/components/atoms/FileInput'
+import ImageArea from '@/components/atoms/ImageArea'
+
+export default {
+    name: 'ImageView',
+    components: {
+        FileInput,
+        ImageArea,
+    },
+    data: function () {
+        return {
+            image: '',
+        }
+    },
+    methods: {
+        /**
+         * ファイルを読み込んだ
+         */
+        onReadFile: function (base64) {
+            this.image = base64
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
