445ff7b0d52d7c897fe2b2cd3c60a4c12da9b9a5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-14 22:08:16 +0900

    画像表示Component

---
+++ frontend/src/components/atoms/ImageArea.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="image-area">
+        <image :src="image" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ImageArea',
+    props: {
+        image: {
+            type: String,
+            default: '',
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
