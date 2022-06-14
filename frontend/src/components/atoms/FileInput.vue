f753cd0b71ab84423d0f6d605ef71595ffd1bd96
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-14 22:05:26 +0900

    読み込んだ画像をBase64に変換して返すComponent

---
+++ frontend/src/components/atoms/FileInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="file-input">
+        <form>
+            <input type="file" accept="image/*" @change="onSelected" />
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FileInput',
+    methods: {
+        /**
+         * 選択された
+         */
+        onSelected: function (e) {
+            const file = e.target.files[0]
+            if (!file) { return }
+
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = () => {
+                this.$emit('on-load', reader.result)
+            }
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
